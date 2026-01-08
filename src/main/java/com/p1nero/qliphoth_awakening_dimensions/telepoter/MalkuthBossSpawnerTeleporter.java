af29a262e1132130dd43f36a2100eb23d80167f9
p1nero <1070402601@qq.com>
2026-01-08 23:48:33 +0800

    fix the wrong rotation

--- src/main/java/com/p1nero/qliphoth_awakening_dimensions/telepoter/MalkuthBossSpawnerTeleporter.java
+++ src/main/java/com/p1nero/qliphoth_awakening_dimensions/telepoter/MalkuthBossSpawnerTeleporter.java
@@ -1,7 +1,5 @@
 package com.p1nero.qliphoth_awakening_dimensions.telepoter;
 
-import com.finderfeed.fdbosses.content.entities.malkuth_boss.malkuth_boss_spawner.MalkuthBossSpawner;
-import com.finderfeed.fdbosses.init.BossEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
@@ -18,14 +16,14 @@ public class MalkuthBossSpawnerTeleporter implements ITeleporter {
     @Override
     public @Nullable PortalInfo getPortalInfo(Entity entity, ServerLevel destinationLevel, Function<ServerLevel, PortalInfo> defaultPortalInfo) {
         BlockPos[] corners = {
-                new BlockPos(40, 63, -40),
-                new BlockPos(-40, 63, -40),
-                new BlockPos(40, 63, 40),
-                new BlockPos(-40, 63, 40)
+                new BlockPos(10, 64, 0),
+                new BlockPos(-10, 64, 0),
+                new BlockPos(0, 64, 10),
+                new BlockPos(0, 64, -10)
         };
 
         for (BlockPos corner : corners) {
-            if (destinationLevel.getBlockState(corner).is(Blocks.BLACKSTONE)) {
+            if (destinationLevel.getBlockState(corner).is(Blocks.BLACKSTONE) && !destinationLevel.getBlockState(corner.above()).is(Blocks.POLISHED_BLACKSTONE_SLAB)) {
                 BlockPos teleportPos = new BlockPos(corner.getX(), 66, corner.getZ());
                 return new PortalInfo(teleportPos.getCenter(), Vec3.ZERO, entity.getYRot(), entity.getXRot());
             }
